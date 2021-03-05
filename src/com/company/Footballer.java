a5acaf8f9459788e6af55732008676cff32eb9fe
Dias <kamzadias31@gmail.com>
2021-03-05 23:35:08 +0600

    Initial Commit

--- src/com/company/Footballer.java
+++ src/com/company/Footballer.java
@@ -11,20 +11,33 @@ public class Footballer implements IAthlete{
         this.goals = goals;
         this.salary = salary;
     }
-    public String checkSalaryByEuros(){
-        if (salary <= 100000) return "You're a deep reserve player";
-        if (salary > 100000 && salary <= 500000) return "You are a reserve player or a promising youth player";
-        if (salary > 500000 && salary <= 1000000) return "You're a player who sometimes comes out to play";
-        if (salary > 1000000 && salary <= 5000000) return "You are already a well-known player and you go out on the field often";
-        if (salary > 50000000) return "You are a man who has a contract like Messi";
-        return "It's time to think about retirement";
+    public String commentsToSalaryByEuros(){
+        if (salary <= 100000 && salary >= 50000){
+            return "You're a deep reserve player";
+        } else if(salary > 100000 && salary <= 500000){
+            return "You are a reserve player or a promising youth player";
+        }else if (salary > 500000 && salary <= 1000000){
+            return "You're a player who sometimes comes out to play";
+        }else if (salary > 1000000 && salary <= 5000000) {
+            return "You are already a well-known player and you go out on the field often";
+        }else if (salary > 50000000){
+            return "You are a man who has a contract like Messi";
+        }else{
+            return "It's time to think about retirement";
+        }
+
     }
 
-    public String checkSeasonQuality(){
-        if (goals > 40) return "You are G.O.A.T";
-        if (goals > 20) return "Perfect season";
-        if (goals > 10) return "Good season";
-        return "It's time to think about retirement";
+    public String seasonQuality(){
+        if (goals > 40){
+            return "You are G.O.A.T";
+        } else if (goals > 20) {
+            return "Perfect season";
+        }else if (goals > 10){
+            return "Good season";
+        }else{
+            return "It's time to think about retirement";
+        }
     }
     @Override
     public void training() {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,23 +6,18 @@ import java.lang.reflect.InvocationTargetException;
 public class Main {
 
     public static void main(String[] args) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchFieldException, NoSuchMethodException{
-        IAthlete iAthlete = new IAthlete() {
-            @Override
-            public void training() {
-                System.out.println("Practicing tactics to become the best in history");
-            }
-        };
+        IAthlete iAthlete = () -> System.out.println("Practicing tactics to become the best in history");
         iAthlete.goal();
         iAthlete.training();
 
-        final Footballer footballer = Footballer.class.getConstructor(int.class, double.class).newInstance(50, 51000000);
+        final Footballer footballer = Footballer.class.getConstructor(int.class, double.class).newInstance(10, 5000000);
         Field footballerGoals = Footballer.class.getDeclaredField("goals");
         Field footballerSalary = Footballer.class.getDeclaredField("salary");
         System.out.println(footballerGoals + " " + footballerSalary);
         System.out.println("---------------");
-        System.out.println(footballer.checkSalaryByEuros());
+        System.out.println(footballer.commentsToSalaryByEuros());
         System.out.println("---------------");
-        System.out.println(footballer.checkSeasonQuality());
+        System.out.println(footballer.seasonQuality());
         System.out.println("---------------");
         System.out.println(footballer);
 
